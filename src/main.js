17cbf1889f73b81803c4f4d6d402e31dd26a8db8
Miklós Galicz <gnick666@gmail.com>
2020-09-21 22:28:34 +0200

    added vue router

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
 import i18n from "./i18n";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   i18n,
+  router,
   render: h => h(App)
 }).$mount("#app");
